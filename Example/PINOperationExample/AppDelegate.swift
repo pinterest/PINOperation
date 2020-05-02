13b15527b5814ce8c43edf5c65bf718e8f05a74d
martinpucik <martin.pucik@me.com>
2020-05-02 11:09:53 +0200

    feat: Swift Package Manager
    Added some code from PINOperation to compile in example project

--- Example/PINOperationExample/AppDelegate.swift
+++ Example/PINOperationExample/AppDelegate.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    private let queue: PINOperationQueue = PINOperationQueue(maxConcurrentOperations: 5)
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        let queue = PINOperationQueue(maxConcurrentOperations: 2)
+        let operationCount = 100
+        let group = DispatchGroup()
+        for _ in 0..<operationCount {
+            group.enter()
+            queue.scheduleOperation({
+                group.leave()
+            }, with: .default)
+        }
+
+        let success = group.wait(timeout: .now() + 20)
+        if success != .success {
+            fatalError("Timed out before completing 100 operations")
+        }
         return true
     }
 }
--- Example/PINOperationExample/MainViewController.m
+++ Example/PINOperationExample/MainViewController.m
@@ -8,26 +8,34 @@
 
 #import "MainViewController.h"
 #import <PINOperation/PINOperation.h>
+//#import <pthread.h>
 
 @interface MainViewController ()
-
+@property (nonatomic, strong) PINOperationQueue *queue;
 @end
 
 @implementation MainViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.queue = [[PINOperationQueue alloc] initWithMaxConcurrentOperations:5];
+
+    const NSUInteger operationCount = 100;
+    dispatch_group_t group = dispatch_group_create();
+
+    for (NSUInteger count = 0; count < operationCount; count++) {
+        dispatch_group_enter(group);
+        [self.queue scheduleOperation:^{
+            dispatch_group_leave(group);
+        } withPriority:PINOperationQueuePriorityDefault];
+    }
+
+    NSUInteger success = dispatch_group_wait(group, [self timeout]);
+    NSAssert(success == 0, @"Timed out before completing 100 operations");
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (dispatch_time_t)timeout {
+    return dispatch_time(DISPATCH_TIME_NOW, (int64_t)(20 * NSEC_PER_SEC));
 }
-*/
 
 @end
