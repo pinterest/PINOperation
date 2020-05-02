adfe3d5e421a982d7a59bf326a710413280ad21f
martinpucik <martin.pucik@me.com>
2020-05-02 10:20:00 +0200

    feat: Swift Package Manager
    SPM tests

--- Package.swift
+++ Package.swift
@@ -13,5 +13,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "PINOperation", path: "Source", publicHeadersPath: "."),
+        .testTarget(name: "PINOperationTests", dependencies: ["PINOperation"], path: "Tests")
     ]
 )
